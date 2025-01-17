741492ece2282c22c1390f07a760508983bc6185
Mordi <Sonoflackar@gmail.com>
2025-01-16 17:11:09 -0800

    Update map.svelte

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -4,12 +4,15 @@
 
 <svelte:head>
   <title>Guardians of Adelia - Map</title>
+  <meta name="description" content="Explore the interactive map of Guardians of Adelia and discover new adventures in this fantasy MMORPG." />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
 
 <div class="container h-screen py-12 mx-auto">
   <iframe
-    class="w-full min-h-full rounded-lg border-4"
-    title="map"
+    class="w-full h-full rounded-lg border-4"
+    title="Interactive Map of Guardians of Adelia"
+    aria-label="Interactive map showcasing the Guardians of Adelia world"
     src="https://master.d2esxhypr1dlpv.amplifyapp.com"
-  />
+  ></iframe>
 </div>
